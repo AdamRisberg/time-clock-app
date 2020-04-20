8c9b572714aa7ab455afa33fd6d62ab28f987e8a
Adam Risberg <myniztan@hotmail.com>
2020-04-20 16:58:23 -0500

    Add react-router to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import TimeClock from "./TimeClock/TimeClock";
+import Timesheet from "./Timesheet/Timesheet";
 
 function App() {
   return (
-    <div className="App">
-      <div>App</div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/timesheets">
+          <Timesheet />
+        </Route>
+        <Route path="/">
+          <TimeClock />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
