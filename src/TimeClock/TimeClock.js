540a67419268c77e5b0d11e4a05accfcbbfd821f
Adam Risberg <myniztan@hotmail.com>
2020-04-20 16:56:47 -0500

    Add basic styling and features to Timeclock

--- src/TimeClock/TimeClock.js
+++ src/TimeClock/TimeClock.js
@@ -1,9 +1,9 @@
 import React from "react";
+import database from "../database";
 import "./TimeClock.css";
 
-function TimeClock() {
+function Clock() {
   const [time, setTime] = React.useState(new Date());
-  const [working, setWorking] = React.useState(false);
 
   React.useEffect(() => {
     const timerId = setInterval(() => {
@@ -14,13 +14,52 @@ function TimeClock() {
   }, []);
 
   return (
+    <div className="TimeClock-time">{time && time.toLocaleTimeString()}</div>
+  );
+}
+
+function TimeClock({ employeeId = 1 }) {
+  const [project, setProject] = React.useState("Standard Work");
+  const [working, setWorking] = React.useState(false);
+
+  function handleClockOut() {
+    database.clockOut(employeeId);
+    setWorking(false);
+    console.log(database.getAllTimeClockData());
+  }
+
+  function handleClockIn() {
+    database.clockIn(employeeId);
+    setWorking(true);
+    console.log(database.getAllTimeClockData());
+  }
+
+  return (
     <div className="TimeClock">
-      <div className="TimeClock-time">{time && time.toLocaleTimeString()}</div>
+      <Clock />
+      <select
+        onChange={(e) => setProject(e.target.value)}
+        value={project}
+        className="TimeClock-project-select"
+        disabled={working}
+      >
+        <option>Select project...</option>
+        <option>Standard Work</option>
+        <option>Maintanence</option>
+      </select>
       <div className="TimeClock-controls">
-        <button className="TimeClock-btn" disabled={working}>
+        <button
+          onClick={handleClockIn}
+          className="TimeClock-btn"
+          disabled={working}
+        >
           Clock In
         </button>
-        <button className="TimeClock-btn" disabled={!working}>
+        <button
+          onClick={handleClockOut}
+          className="TimeClock-btn"
+          disabled={!working}
+        >
           Clock Out
         </button>
       </div>
